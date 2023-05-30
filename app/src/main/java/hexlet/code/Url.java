45797f80fad0b333825d4a85f9e161b010c59ce2
evil_face <den.bragin@gmail.com>
2023-05-30 14:07:49 +0600

    add Url.java entity

---
+++ app/src/main/java/hexlet/code/Url.java
@@ -0,0 +1,46 @@
+package hexlet.code;
+
+import io.ebean.Model;
+import io.ebean.annotation.WhenCreated;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.Instant;
+
+@Entity
+public final class Url extends Model {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String name;
+
+    @WhenCreated
+    private Instant createdAt;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long newId) {
+        this.id = newId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant newCreatedAt) {
+        this.createdAt = newCreatedAt;
+    }
+}
