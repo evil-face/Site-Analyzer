1a9318bc5e3f92955018c2ff8f5e4802fc2e93f5
evil_face <den.bragin@gmail.com>
2023-05-30 21:32:01 +0600

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -34,7 +34,7 @@ public class App {
     }
 
     public static void main(String[] args) throws ClassNotFoundException {
-        Class.forName("org.postgresql.Driver");
+//        Class.forName("org.postgresql.Driver");
         Javalin app = getApp();
         app.start(getPort());
 
