31c37aa073bc03bd8596546508af049adc519b4e
evil_face <den.bragin@gmail.com>
2023-05-30 14:06:40 +0600

    update App.java with dev and prod modes

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,20 +1,45 @@
 package hexlet.code;
 
+import hexlet.code.query.QUrl;
+import io.ebean.DB;
 import io.javalin.Javalin;
 
 public class App {
+
+    private static final String DEFAULT_PORT = "8093";
+    private static final String DEV_MODE = "dev";
+    private static final String PROD_MODE = "prod";
     private static int getPort() {
-        String port = System.getenv().getOrDefault("PORT", "8093");
+        String port = System.getenv().getOrDefault("PORT", DEFAULT_PORT);
         return Integer.parseInt(port);
     }
 
+    private static String getMode() {
+        return System.getenv().getOrDefault("APP_ENV", DEV_MODE);
+    }
+
+    private static boolean isProduction() {
+        return getMode().equals(PROD_MODE);
+    }
+
     public static Javalin getApp() {
-        return Javalin.create(config -> config.plugins.enableDevLogging())
-                .get("/", ctx -> ctx.result("Hello World"));
+        Javalin app = Javalin.create(config -> {
+            if (!isProduction()) {
+                config.plugins.enableDevLogging();
+            }
+        });
+
+        app.get("/", ctx -> ctx.result("Hello World"));
+
+        return app;
     }
 
     public static void main(String[] args) {
         Javalin app = getApp();
         app.start(getPort());
+
+        Url testUrl = new Url();
+        testUrl.setName("testUrl");
+        DB.save(testUrl);
     }
 }
