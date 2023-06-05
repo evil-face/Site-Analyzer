3740662979c4f113f637899d0e302fbb2b8607db
evil_face <den.bragin@gmail.com>
2023-06-05 14:49:41 +0600

    delete RootController.java

--- app/src/main/java/hexlet/code/controllers/RootController.java
+++
@@ -1,9 +0,0 @@
-package hexlet.code.controllers;
-
-import io.javalin.http.Handler;
-
-public class RootController {
-    public static Handler getIndex = ctx -> {
-        ctx.render("index.html");
-    };
-}
