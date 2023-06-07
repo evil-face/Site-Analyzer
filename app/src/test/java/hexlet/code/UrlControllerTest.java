8e84746eec7a0aa83273bab49ebc9f334b89c587
evil_face <den.bragin@gmail.com>
2023-06-07 14:44:38 +0600

    create UrlControllerTest.java with tests

---
+++ app/src/test/java/hexlet/code/UrlControllerTest.java
@@ -0,0 +1,177 @@
+package hexlet.code;
+
+import hexlet.code.query.QUrl;
+import io.ebean.DB;
+import io.ebean.Transaction;
+import io.javalin.Javalin;
+import io.javalin.http.HttpStatus;
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class UrlControllerTest {
+    private Transaction transaction;
+    private final static String FIRST_PAGE_EXISTING_URL = "http://yandex.ru";
+    public final static String SECOND_PAGE_EXISTING_URL = "http://secondpage.org";
+
+    @BeforeAll
+    void beforeAll() {
+        Javalin app = App.getApp();
+        app.start(0);
+        int port = app.port();
+        Unirest.config().defaultBaseUrl("http://localhost:" + port);
+    }
+
+    @BeforeEach
+    void setUp() {
+        transaction = DB.beginTransaction();
+    }
+
+    @AfterEach
+    void tearDown() {
+        transaction.rollback();
+    }
+
+    @Test
+    void testGetAllDefaultPage() {
+        Url expectedOne = new QUrl().id.eq(1).findOne();
+        Url expectedTwo = new QUrl().id.eq(2).findOne();
+
+        HttpResponse<String> response = Unirest.get("/urls").asString();
+        assertThat(response.getStatus()).isEqualTo(200);
+
+        assertThat(response.getBody()).contains(
+                "Сайты",
+                "Назад",
+                "Вперед");
+
+        assertThat(response.getBody()).contains(
+                expectedOne.getName(),
+                String.valueOf(expectedOne.getId()));
+
+        assertThat(response.getBody()).contains(
+                expectedTwo.getName(),
+                String.valueOf(expectedTwo.getId()));
+
+        assertThat(response.getBody()).doesNotContain(SECOND_PAGE_EXISTING_URL);
+    }
+
+    @Test
+    void testGetAllSecondPage() {
+        HttpResponse<String> response = Unirest.get("/urls")
+                .queryString("page", 2)
+                .asString();
+        assertThat(response.getStatus()).isEqualTo(200);
+
+        assertThat(response.getBody()).contains(SECOND_PAGE_EXISTING_URL);
+
+        assertThat(response.getBody()).doesNotContain(
+                FIRST_PAGE_EXISTING_URL);
+    }
+
+    @Test
+    void testGetOneCorrect() {
+        Url expected = new QUrl().name.eq(SECOND_PAGE_EXISTING_URL).findOne();
+
+        HttpResponse<String> response = Unirest.get("/urls/" + expected.getId())
+                .asString();
+
+        assertThat(response.getStatus()).isEqualTo(200);
+
+        assertThat(response.getBody()).contains(
+                "Дата создания",
+                "Проверки",
+                "Запустить проверку");
+
+        assertThat(response.getBody()).contains(
+                String.valueOf(expected.getId()),
+                expected.getName());
+    }
+
+    @Test
+    void testGetOneBadRequest() {
+        String expected = "String ID must be numeric";
+        HttpResponse<String> response = Unirest.get("/urls/badreq").asString();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.getCode());
+        assertThat(response.getBody()).isEqualTo(expected);
+    }
+
+    @Test
+    void testGetOneNotFound() {
+        String expected = "Not Found";
+        HttpResponse<String> response = Unirest.get("/urls/12345").asString();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.NOT_FOUND.getCode());
+        assertThat(response.getBody()).isEqualTo(expected);
+    }
+
+    @Test
+    void testCreateCorrect() {
+        String userInput = "http://www.uniquesite.com/path";
+        HttpResponse<String> response = Unirest.post("/urls")
+                .field("url", userInput)
+                .asString();
+        Url actual = new QUrl().name.eq("http://uniquesite.com").findOne();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.CREATED.getCode());
+        assertThat(actual).isNotNull();
+
+        assertThat(response.getBody()).contains("Страница успешно добавлена");
+    }
+
+    @Test
+    void testCreateBadRequestBlank() {
+        String userInput = " ";
+        List<Url> listBefore = new QUrl().findList();
+        HttpResponse<String> response = Unirest.post("/urls")
+                .field("url", userInput)
+                .asString();
+        List<Url> listAfter = new QUrl().findList();
+        Url actual = new QUrl().name.eq(userInput).findOne();
+
+        assertThat(listBefore).hasSize(listAfter.size());
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.getCode());
+        assertThat(actual).isNull();
+        assertThat(response.getBody()).contains("Некорректный URL");
+    }
+
+    @Test
+    void testCreateBadRequestMalformed() {
+        String userInput = "www.malformed.com";
+        List<Url> listBefore = new QUrl().findList();
+        HttpResponse<String> response = Unirest.post("/urls")
+                .field("url", userInput)
+                .asString();
+        List<Url> listAfter = new QUrl().findList();
+        Url actual = new QUrl().name.eq(userInput).findOne();
+
+        assertThat(listBefore).hasSize(listAfter.size());
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.getCode());
+        assertThat(actual).isNull();
+        assertThat(response.getBody()).contains("Некорректный URL");
+    }
+
+    @Test
+    void testCreateExistingEntity() {
+        String userInput = SECOND_PAGE_EXISTING_URL;
+        List<Url> listBefore = new QUrl().findList();
+        HttpResponse<String> response = Unirest.post("/urls")
+                .field("url", userInput)
+                .asString();
+        List<Url> listAfter = new QUrl().findList();
+
+        assertThat(listBefore).hasSize(listAfter.size());
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.getCode());
+        assertThat(response.getBody()).contains("Страница уже существует");
+    }
+}
