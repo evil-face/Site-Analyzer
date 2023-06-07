ba7bc2cd4cd34ff520d1f0d07f38b3c45ec093f8
evil_face <den.bragin@gmail.com>
2023-06-07 14:44:44 +0600

    create UrlTest.java with tests

---
+++ app/src/test/java/hexlet/code/UrlTest.java
@@ -0,0 +1,34 @@
+package hexlet.code;
+
+import hexlet.code.query.QUrl;
+import io.ebean.DB;
+import io.ebean.Transaction;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public final class UrlTest {
+    private Transaction transaction;
+
+    @BeforeEach
+    void setUp() {
+        transaction = DB.beginTransaction();
+    }
+
+    @AfterEach
+    void tearDown() {
+        transaction.rollback();
+    }
+
+    @Test
+    void testUrlCreation() {
+        Url expected = new Url("testName2");
+        expected.save();
+        Url actual = new QUrl().id.eq(expected.getId()).findOne();
+
+        assertThat(actual.getName()).isEqualTo(expected.getName());
+        assertThat(actual.getId()).isGreaterThan(0);
+        assertThat(actual.getCreatedAt()).isNotNull();
+    }
+}
