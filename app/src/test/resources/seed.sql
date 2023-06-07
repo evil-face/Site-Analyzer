c1a7b727acc8522147cbb7286de4f24405a689ca
evil_face <den.bragin@gmail.com>
2023-06-07 14:44:24 +0600

    create seed.sql with test setup

---
+++ app/src/test/resources/seed.sql
@@ -0,0 +1,13 @@
+INSERT INTO url (name, created_at) VALUES
+	('http://yandex.ru', CURRENT_TIMESTAMP()),
+	('http://google.com', CURRENT_TIMESTAMP()),
+	('http://test3.com', CURRENT_TIMESTAMP()),
+	('http://test4.com', CURRENT_TIMESTAMP()),
+	('http://test5.com', CURRENT_TIMESTAMP()),
+	('http://test6.com', CURRENT_TIMESTAMP()),
+	('http://test7.com', CURRENT_TIMESTAMP()),
+	('http://test8.com', CURRENT_TIMESTAMP()),
+	('http://test9.com', CURRENT_TIMESTAMP()),
+	('http://test10.com', CURRENT_TIMESTAMP()),
+	('http://test11.com', CURRENT_TIMESTAMP()),
+	('http://secondpage.org', CURRENT_TIMESTAMP());
